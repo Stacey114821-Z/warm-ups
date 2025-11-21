87151ea45238d841040f699130e07302c674e364
Stacey114821-Z <stacey114821@mcpsva.org>
2025-11-21 14:39:47 -0500

    Update Warm-Up_4_QC_Hello.py

--- Warm-Up_4_QC_Hello.py
+++ Warm-Up_4_QC_Hello.py
@@ -1,7 +1,7 @@
 """
 Filename: Warm-Up_4_QC_Hello.py
-Author: <Lastname, Firstname>
-Created: <MM/DD/YYYY>
+Author: <Zavala, Stacey>
+Created: <11/21/2025>
 Instructor: Holtslander
 """
 
@@ -11,9 +11,12 @@ def hello():
     "Hello, <name>"
     :return: None
     """
+    name = input("what is your name"/n")
+    print(f"hello, (name)"
     ### YOUR CODE GOES HERE ###
 
 
 ### YOU SHOULD NOT NEED TO CHANGE ANYTHING HERE ###
 if __name__ == '__main__':
+
     hello()
